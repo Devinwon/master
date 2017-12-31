b3253b1e2904aaedc9b86079d6e9b142109d9a04
wonsynpo <reaptem@gmail.com>
2017-12-31 23:20:18 +0800

    coding

--- coding-exercise/getPrize.py
+++ coding-exercise/getPrize.py
@@ -1,5 +1,13 @@
 import random
+import time
+namelist=['张三','网商','华德','学友','雪亮','雪莲','丛丛','devin','jeson','green']
+prize_item={'一等奖':1,"二等奖":2,"三等奖":3}
 
-namelist=['张三','网商','华德','学友','雪亮','雪莲','丛丛']
 nmlist=namelist[:]
-print(nmlist)
+user_gotprize=[]
+
+for cnt in range(6):
+	# r=int(random.random()*10)%len(namelist)
+	r=random.randint(0,len(namelist)-1)
+	time.sleep(3)
+	print(namelist.pop(r))
