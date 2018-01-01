d03a66339907654d8ee19ab3448268b78ebd0d9d
wonsynpo <reaptem@gmail.com>
2018-01-01 11:02:43 +0800

    gotprize

--- coding-exercise/getPrize.py
+++ coding-exercise/getPrize.py
@@ -1,13 +1,20 @@
-import random
-import time
-namelist=['张三','网商','华德','学友','雪亮','雪莲','丛丛','devin','jeson','green']
-prize_item={'一等奖':1,"二等奖":2,"三等奖":3}
 
-nmlist=namelist[:]
-user_gotprize=[]
+import random,time
 
-for cnt in range(6):
-	# r=int(random.random()*10)%len(namelist)
-	r=random.randint(0,len(namelist)-1)
-	time.sleep(3)
-	print(namelist.pop(r))
+# list can keep the order
+prize_item=[["三等奖",3],["二等奖",2],["一等奖",1]]
+user=["devin","bob","matina","jesson","hank","frank","green","white","blue","natting","kin","jon"]
+
+# user_bak=user[:]
+lucky_dog=[]
+print("\n下面开始抽奖，请保持关注\n")
+for v in prize_item:
+	time.sleep(2)
+	print("\n开始抽取"+v[0]+"......\n")
+	for cnt in range(v[1]):
+		time.sleep(2)
+		nm=user.pop(random.randint(0,len(user)-1))
+		lucky_dog.append(nm) 
+		print("恭喜"+v[0]+"获得者："+nm)
+
+print("\n恭喜以上"+str(len(lucky_dog))+"位获奖者")
