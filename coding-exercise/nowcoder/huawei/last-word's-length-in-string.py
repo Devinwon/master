108294ed6702ce94e4d07a6c7066f4d588824a63
Devin <917570905@qq.com>
2018-03-09 22:06:04 +0800

    len of last word in string

---
+++ coding-exercise/nowcoder/huawei/last-word's-length-in-string.py
@@ -0,0 +1,21 @@
+'''
+#description:#
+计算字符串最后一个单词的长度，单词以空格隔开。
+
+###Input description###
+一行字符串，非空，长度小于5000。
+
+###Output description###
+整数N，最后一个单词的长度。
+
+#example#
+
+###Input###
+hello world
+
+###Output###
+5
+'''
+
+str = input().strip().split()
+print(len(str[len(str)-1]))
