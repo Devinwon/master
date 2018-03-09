bcf426875737ff33119c4bcd581d0c45589642be
Devin <917570905@qq.com>
2018-03-09 22:31:32 +0800

    get-no-repeat-number

---
+++ coding-exercise/nowcoder/huawei/get-no-repeat-number.py
@@ -0,0 +1,28 @@
+'''
+#description:#
+输入一个int型整数，
+按照从右向左的阅读顺序，
+返回一个不含重复数字的新的整数。
+
+###Input description###
+输入一个int型整数
+
+###Output description###
+按照从右向左的阅读顺序，返回一个不含重复数字的新的整数
+
+#example#
+
+###Input###
+9876673
+
+###Output###
+37689
+'''
+
+num=input()
+rel=""
+for v in num[::-1]:
+	if v not in rel:
+		rel+=v
+
+print(rel)
