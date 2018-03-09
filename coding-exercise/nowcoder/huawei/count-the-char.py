3c3f37ae0c44647b18ed8918d2784701f3765c3b
Devin <917570905@qq.com>
2018-03-09 22:20:17 +0800

    count the char

---
+++ coding-exercise/nowcoder/huawei/count-the-char.py
@@ -0,0 +1,26 @@
+'''
+#description:#
+写出一个程序，接受一个有字母和数字以及空格组成的字符串，
+和一个字符，然后输出输入字符串中含有该字符的个数。
+不区分大小写。
+
+###Input description###
+输入一个有字母和数字以及空格组成的字符串，和一个字符。
+
+###Output description###
+输出输入字符串中含有该字符的个数。
+
+#example#
+
+###Input###
+ABCDEF A
+
+###Output###
+1
+'''
+
+s=input()
+c=input()
+s=s.upper()
+c=c.upper()
+print(s.count(c))
