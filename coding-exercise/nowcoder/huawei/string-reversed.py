8e48f7f14e47c5787bbd01aef697807a8f575ddf
Devin <917570905@qq.com>
2018-03-10 13:35:38 +0800

    string reversed

---
+++ coding-exercise/nowcoder/huawei/string-reversed.py
@@ -0,0 +1,21 @@
+'''
+#description:#
+写出一个程序，接受一个字符串，然后输出该字符串反转后的字符串。例如：
+
+###Input description###
+输入N个字符
+
+###Output description###
+输出该字符串反转后的字符串
+
+#example#
+
+###Input###
+abcd
+
+###Output###
+dcba
+'''
+
+s=input()
+print(s[::-1])
