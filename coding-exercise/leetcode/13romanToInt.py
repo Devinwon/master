5f00ae6f323bc5dfcca14489db14335aba07a8c0
Devin <917570905@qq.com>
2018-03-11 23:41:34 +0800

    remove elements

---
+++ coding-exercise/leetcode/13romanToInt.py
@@ -0,0 +1,15 @@
+'''
+13. 罗马数字转整数
+
+给定一个罗马数字，将其转换成整数。
+返回的结果要求在 1 到 3999 的范围内。
+'''
+
+
+
+class Solution:
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
