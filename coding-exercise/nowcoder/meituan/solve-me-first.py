2ba0147ab47c1e0fc46411ae28ceb3b73614ad32
Devin <917570905@qq.com>
2018-04-22 15:44:16 +0800

    solve me first mark

--- coding-exercise/nowcoder/meituan/solve-me-first.py
+++ coding-exercise/nowcoder/meituan/solve-me-first.py
@@ -1,3 +1,25 @@
+"""
+Complete the function solveMeFirst to compute the sum of two integers.
+
+Function prototype:
+
+int solveMeFirst(int x, int y);
+
+where,
+x is the first integer input.
+y is the second integer input
+Return values
+
+sum of the above two integers
+
+Sample Input
+x = 2
+y = 3
+
+Sample Output
+5
+
+"""
 def solveMeFirst(a,b):
    # Hint: Type return a+b below 
    return a+b
