de09e7b41258495e9ea7c2ad1a004748cc8b869e
Devin <917570905@qq.com>
2018-05-01 16:43:51 +0800

    swap-case

---
+++ coding-exercise/hankerRank/python/string/swap-case.py
@@ -0,0 +1,21 @@
+"""
+https://www.hackerrank.com/challenges/swap-case/problem
+"""
+def swap_case(s):
+	rel=''
+	for v in s:
+		if 'A'<=v<='Z':
+			rel+=v.lower()
+		elif 'a'<=v<='z':
+			rel+=v.upper()
+		else:
+			rel+=v
+	return rel
+
+# if __name__ == '__main__':
+# 	s = input()
+# 	result = swap_case(s)
+# 	print(result)
+
+
+print(swap_case('wwW.CCnu]-'))
