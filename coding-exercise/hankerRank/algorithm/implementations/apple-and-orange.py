d54da3875dee8254e54d22f08cbb4318f4a2d82d
Devin <917570905@qq.com>
2018-05-05 20:00:33 +0800

    apple-and-orange

--- coding-exercise/hankerRank/algorithm/implementations/apple-and-orange.py
+++ coding-exercise/hankerRank/algorithm/implementations/apple-and-orange.py
@@ -2,3 +2,40 @@
 https://www.hackerrank.com/challenges/apple-and-orange/problem
 
 """
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the countApplesAndOranges function below.
+#
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    #
+    # Write your code here.
+    #
+
+if __name__ == '__main__':
+    st = input().split()
+
+    s = int(st[0])
+
+    t = int(st[1])
+
+    ab = input().split()
+
+    a = int(ab[0])
+
+    b = int(ab[1])
+
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    apple = list(map(int, input().rstrip().split()))
+
+    orange = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apple, orange)
