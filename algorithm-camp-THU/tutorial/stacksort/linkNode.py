ca63c00038c5a2828aee4a0e3e1f14a7a5655f74
Devin <917570905@qq.com>
2018-05-20 14:56:16 +0800

    linkNode traverse done

--- algorithm-camp-THU/tutorial/stacksort/linkNode.py
+++ algorithm-camp-THU/tutorial/stacksort/linkNode.py
@@ -2,7 +2,6 @@ class LinkNode():
 	"""docstring for LinkNode
 	default parameter located in __init__(),not essential 
 	linkList store students' score
-	
 
 	def __init__(self,score=0,name=None):
 		self.score = score
@@ -28,18 +27,20 @@ for _ in range(total):
 	data=LinkNode()
 	data.name=input("学生姓名: ")
 	data.score=input("学生成绩: ")
-	
 	ptr.next=data 	# 存储指针为新元素所在的位置
-
+	print()
 	data.next=None	# 下一个元素的next设置为None
 	ptr=ptr.next		# 移动(更新)ptr
 
 # 遍历单项链表
 
-ptr=head	# 从链表的头部开始
+print("姓名\t成绩")
+ptr=head.next			# 从链表的头部开始,注意这里不能是head,head中存储的是初始化数据
 while ptr!=None:
-	print("学生姓名\t学生成绩")
-	pass
+	nm=ptr.name
+	score=ptr.score
+	print("%s  %s"%(nm,score))
+	ptr=ptr.next # 移动(更新)ptr
 
 
 		
