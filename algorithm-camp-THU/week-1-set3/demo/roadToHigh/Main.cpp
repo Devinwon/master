53e186e1cb54ab3053972394ca45b57ec6266269
Devin <917570905@qq.com>
2018-05-21 23:03:43 +0800

    roadToHigh done

--- algorithm-camp-THU/week-1-set3/demo/roadToHigh/Main.cpp
+++ algorithm-camp-THU/week-1-set3/demo/roadToHigh/Main.cpp
@@ -5,20 +5,29 @@ using namespace std;
 
 /* 请在这里定义你需要的全局变量 */
 const int N=500005;
-struct UnionSet
+class UnionSet
 {
+public:
     int f[N];
 
     void init(int n){
+        for (int i=1;i<=n;++i)
+            f[i]=i;
 
     } 
 
     int find(int x){
-
+        return f[x]==x?x:f[x]=find(f[x]);
     }
 
     bool merge(int x,int y){
-
+        int setX=find(x);
+        int setY=find(y);
+        if (setX!=setY){
+            f[setX]=setY;
+            return true;
+        }
+        return false;
     }
 }us;
 
@@ -29,15 +38,19 @@ struct UnionSet
 // U：大小为m的数组，表示m条边的其中一个端点
 // V：大小为m的数组，表示m条边的另一个端点
 // 返回值：所有需要升级的边，从小到大排列；第一小问的答案自然即为返回值的size，所以你不必考虑如何返回size
+/*及人体思路:
+
+边权大小排序,
+并查集维护连通性
+*/
 vector<int> getAnswer(int n, int m, vector<int> U, vector<int> V) {
     /* 请在这里设计你的算法 */
     vector<int> ans;
     us.init(n);
-    for()
+    for(int i=m-1;i>=0;--i)
         if(us.merge(U[i],V[i]))
-            ans.push_back();
-
-
+            ans.push_back(i+1);
+    reverse(ans.begin(),ans.end());
     return ans;
 
 }
