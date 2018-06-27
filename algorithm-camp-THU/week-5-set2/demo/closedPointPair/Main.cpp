8ea87e97b443ba14cfa94a838b1a590bbd3c3aa5
Devin <917570905@qq.com>
2018-06-27 10:51:12 +0800

    add var func

--- algorithm-camp-THU/week-5-set2/demo/closedPointPair/Main.cpp
+++ algorithm-camp-THU/week-5-set2/demo/closedPointPair/Main.cpp
@@ -32,6 +32,46 @@ lf dis(const ip &a, const ip &b) {
 }
 
 /* 请在这里定义你需要的全局变量 */
+// f最终答案
+lf ans;
+
+// 分治求最近点对
+// l,r表示闭区间[l,r]
+void solve(int l,int r){
+    // 边界情况
+    if(r-l<=1){
+        // blank ---1,只有一个点或只有两个点,如何更新答案
+        return ;
+    }
+
+    //分治计算两遍
+
+    int mid=(l+r)>>1;
+    int md=a[mid].x;    //中间值
+
+    solve(l,mid);
+    solve(mid+1,r);
+
+    // 对y轴进行归并排序,并且去掉,与中间点mid的距离(x轴),比答案要大的点
+    int cnt=0;
+    for(int i=l;j=mid+1;i<=mid||j<=r;){
+        for(;i<=mid&&md-a[i].x>ans;;++i);
+        for(;j<=r&&a[j].x-md>ans;++j);
+        if(i<=mid&&(j>r||a[i].y<a[j].y))
+            b[cnt++]=a[i++];
+        else
+            b[cnt++]=a[j++];
+    }
+
+    //现在b数组里的点是按y轴升序的,根据结论,一个点周围不会超过八个点,因此j不会循环很多次,复杂度得到保证
+    for(int i=0;i<cnt;++i)
+        for(int j=i+1;j<cnt&&b[j].y-b[i].y<=ans;++j)
+            ans=min(ans,dis(b[i],b[j]));
+    // 对y轴进行归并排序,不去掉任何点
+    cnt=0;
+    // blank---2
+    memcpy(a+l,b,sizeof(ip)*cnt);
+}
 
 // 计算最近点对的距离
 // n：n个点
