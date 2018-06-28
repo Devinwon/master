ad5f7529dd1a260abbc3bf76adacacaab61da90e
Devin <917570905@qq.com>
2018-06-28 16:06:54 +0800

    pullwater

--- algorithm-camp-THU/week-3-set3/Fetchingwater/demo/Main.cpp
+++ algorithm-camp-THU/week-3-set3/Fetchingwater/demo/Main.cpp
@@ -4,29 +4,40 @@ using namespace std;
 // ================= 代码实现开始 =================
 
 /* 请在这里定义你需要的全局变量 */
-typedef pair<int,int > pii;
-#define fi first;
-#define se second;
+typedef pair<int,int> pii;
+#define fi first
+#define se second
 
 const int N=2003;
+// mind:mind[i][j]表示从初始状态到达状态(i,j)所需要的最少步骤
+// q:用数组模拟的队列
+// qh:队头下标
+// qt:队尾下标
+
 int mind[N][N];
 pii q[N*N];
-int qh,qt; 
+int qh,qt;
+
+//倒水函数,表示状态p经过第k种策略倒水后的状态
+//p,初始状态
+//k,策略(在下面进行详细解释)
+// n,m两杯子容量
+//返回值
 
 pii to(pii p,int k, int n, int m){
-	if(k==0)
+	if(k==0)		//倒空杯子一
 		return pii(0,p.se);
-	else if (k==1)
+	else if (k==1)	//倒空杯子二
 		return pii(p.fi,0);
-	else if(k==2)
+	else if(k==2)		//倒满杯子一
 		return pii(n,p.se);
-	else if(k==3)
+	else if(k==3)		//倒满杯子二
 		return pii(p.fi,m);
-	else if(k==4)
-		return pii( min(p.fi+p.se,n),max(p.fi+p.se-n,0));
-	else if (k==5)
-		return pii(     );
-	else
+	else if(k==4)		//将杯子二的水向杯子一倒
+		return pii(min(p.fi+p.se,n),max(p.fi+p.se-n,0));
+	else if (k==5)		//将杯子一的水向杯子二倒
+		return pii(max(p.fi+p.se-m,0),min(p.fi+p.se,m));
+	else		//否则什么也不做
 		return p;
 }
 
@@ -42,21 +53,19 @@ int getAnswer(int n, int m, int t, int d) {
 
 	// 进行BFS
 	while(qh<qt){
-		pii u=q[++qh];
+		pii u=q[++qh];		//取出队头元素
 		if(mind[u.fi][u.se]==t)
-			break;
-		for(int k=0;k<6;++k){
+			break;		//如果已经进行了t步,那么没有必要继续搜索了,退出循环即可
+		for(int k=0;k<6;++k){	//枚举6中策略
 			pii v=to(u,k,n,m);
-			if( mind[v.fi][v.se]!=-1)
+			if( mind[v.fi][v.se]!=-1)		//判断目标状态是否未曾到达过
 				continue;
-			q[++qt]=v;
-			mind[v.fi][v.se]=mind[u.fi][u.se]+1;
-
-
+			q[++qt]=v;		//加入队列
+			mind[v.fi][v.se]=mind[u.fi][u.se]+1;	//记录mind
 		}
 	}
 
-	// 统计答案
+	// 统计答案,找到最优的状态
 	int ans=d;
 	for(int i=0;i<=n;++i)
 		for(int j=0;j<=m;++j)
