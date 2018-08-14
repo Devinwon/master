b32d5998441103d1a746ab4aea43bf1f5ac0a62c
Devin <917570905@qq.com>
2018-08-14 20:37:22 +0800

    lace-recursion

---
+++ computer-science-and-python-programing-edX/mid-exam/quiz-code-graders/problem5.py
@@ -0,0 +1,18 @@
+def laceStrings(s1, s2):
+    """
+    s1 and s2 are strings.
+
+    Returns a new str with elements of s1 and s2 interlaced,
+    beginning with s1. If strings are not of same length, 
+    then the extra elements should appear at the end.
+    """
+    # Your Code Here
+    commanLen=min(len(s1),len(s2))
+    rel=""
+    for i in range(commanLen):
+        rel+=s1[i]+s2[i]
+    if len(s1)>commanLen:
+        rel+=s1[commanLen:]
+    elif len(s2)>commanLen:
+        rel+=s2[commanLen:]
+    return rel
