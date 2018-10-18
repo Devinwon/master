1adb06fda9ac3a8932920787e074deb274e23495
Devin <917570905@qq.com>
2018-10-18 16:49:13 +0800

    dequeue

--- coding-exercise/CCF/201712-2.py
+++ coding-exercise/CCF/201712-2.py
@@ -8,23 +8,64 @@ k:表示淘汰参数(报数为k倍数或者末尾为k)
 条件使用函数或者简单语句均可
 
 换一种思路
-"""
+# done-----------------------------
 n,k=list(map(int,input().strip().split()))
-peoplelst=[n for n in range(1,n+1)]
-start=0
+# 1表示都在圈内，0表示不在圈内，下标表示人员编号即可
+peoplelst=[1 for n in range(n)]
+count=0
+flag=True
+while flag:
+	for inx in range(n):
+		if peoplelst[inx]!=0:
+			count+=1
+			if count%k==0 or count%10==k :
+				peoplelst[inx]=0
+				# 去掉了人后，需要计算当前剩余人数 
+				if sum(peoplelst)==1:
+					flag=False
+					break
+for inx in range(n):
+	if peoplelst[inx] :
+		print(inx+1)
+# -----------------------------
+"""
+# 通过使用队列实现
+# -----------------------------
+class Dequeue():
+	def __init__(self):
+		self.items=[]
 
-while n>1:
-	peoplelstbak=peoplelst[::] 
-	for inx in range(1+start,n+1+start):
-		if inx%10==k or inx%k==0:
-			# peoplelst已经改变了,不能对应原来的索引
-			peoplelst.remove(peoplelstbak[inx-start-1])
-			n-=1
-	print('每轮循环完后,数据',peoplelst)
-	print('每轮循环完后,数据bak',peoplelstbak)
-	start+=len(peoplelstbak)
-	# print('start,n',start,n)
-print(peoplelst[0])
+	def size(self):
+		return len(self.items)
+
+	# 出队
+	def deque(self):
+		if len(self.items):
+			t=self.items[0]
+			self.items.remove(self.items[0])
+		return t
+
+	# 入队
+	def enque(self,var):
+		self.items.append(var)
+
+n,k=list(map(int,input().strip().split()))
+peoplelst=Dequeue()
+
+for i in range(n):
+	peoplelst.enque(i+1)
+
+count=0
+while peoplelst.size()>1:
+	count+=1
+	if count%k==0 or count%10==k :
+		peoplelst.deque()
+	else:
+		peoplelst.enque(peoplelst.deque())
+
+print(peoplelst.deque())
+
+
 
 
 
--- coding-exercise/CCF/test.py
+++ coding-exercise/CCF/test.py
@@ -1,4 +1,7 @@
-for i in range(4,-1,-1):
-	print(i)
+lst=[1,2,34]
 
+t=lst[0]
+lst=lst[1:]
+print(t)
+print(lst)
 
